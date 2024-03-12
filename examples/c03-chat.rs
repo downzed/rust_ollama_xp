a66489b6512bf62f9fbbba8118c2e7b078b8e206
Ziv Zerr <downzed@gmail.com>
2024-03-12 14:56:10 +0100

    feat: extended chat

--- examples/c03-chat.rs
+++ examples/c03-chat.rs
@@ -1,7 +1,10 @@
+use std::process;
+
 use ollama_rs::{
     generation::chat::{request::ChatMessageRequest, ChatMessage, MessageRole},
     Ollama,
 };
+use tokio::io::{self as tokio_io, AsyncBufReadExt, AsyncWriteExt, BufReader}; // Import for async read
 
 use xp_ollama::{
     consts::{DEFAULT_SYSTEM_MOCK, MODEL},
@@ -11,21 +14,35 @@ use xp_ollama::{
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // By default it will connect to localhost:11434
     let ollama = Ollama::default();
-    let prompts = &[
-        "What was the color of Napoleon's black hamster?",
-        "What is the best programming language? (be short!), (it's rust btw)",
-        "What was my last question?",
-    ];
 
     let system_msg = ChatMessage::new(MessageRole::System, DEFAULT_SYSTEM_MOCK.to_string());
 
+    // Convert the iterator to an asynchronous stream
+    let stdin = tokio_io::stdin();
+    let mut stdout = tokio_io::stdout();
+
     let mut thread_messages: Vec<ChatMessage> = vec![system_msg];
 
-    for p in prompts {
-        println!("\n [Prompt]: {p}");
-        let prompt_msg = ChatMessage::new(MessageRole::User, p.to_string());
+    let reader = BufReader::new(stdin);
+    let mut lines = reader.lines();
+
+    print_ascii();
+
+    println!();
+    println!("Ask away >> ");
+
+    while let Some(line) = lines.next_line().await? {
+        stdout.flush().await?;
+
+        let prompt = line.trim().to_string();
+
+        if prompt == "exit" {
+            println!("Exiting...");
+            process::exit(0);
+        }
+
+        let prompt_msg = ChatMessage::new(MessageRole::User, prompt);
 
         thread_messages.push(prompt_msg);
 
@@ -34,10 +51,23 @@ async fn main() -> Result<()> {
         let msg_content = stream_chat_print(&ollama, chat_req).await?;
 
         if let Some(content) = msg_content {
-            let asst_msg = ChatMessage::new(MessageRole::Assistant, content);
+            let asst_msg = ChatMessage::new(MessageRole::Assistant, content.clone());
             thread_messages.push(asst_msg);
         }
+        println!();
+        println!("Ask away >> ");
     }
-
     Ok(())
+}
+
+fn print_ascii() {
+    println!(
+        r#"  
+  ___ _               
+ / __| |_ _____ _____ 
+ \__ \  _/ -_) V / -_)
+ |___/\__\___|\_/\___|
+                      "#
+    );
+    println!("\n High! Type the magic word to end the conversation.\n");
 }
